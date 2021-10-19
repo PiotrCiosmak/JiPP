6bb46d554352500cc0b35d6b97d3952284503ac6
Piotr Ciosmak <MrChick888@gmail.com>
2021-10-19 14:11:51 +0200

    zadanie zrobione

--- Lab 3 - Zadanie 2/main.cpp
+++ Lab 3 - Zadanie 2/main.cpp
@@ -6,22 +6,21 @@ using namespace std;
 int losuj_z_przedzialu(int poczatek, int koniec);
 
 int main() {
-    cout<<"Losowanie liczby z podanego przedzialu\n";
+    cout << "Losowanie liczby z podanego przedzialu\n";
 
     int poczatek_przedzialu;
-    cout<<"Podaj poczatek przedzialu:";
-    cin>>poczatek_przedzialu;
+    cout << "Podaj poczatek przedzialu:";
+    cin >> poczatek_przedzialu;
 
     int koniec_przedzialu;
-    cout<<"Podaj koniec przedzialu:";
-    cin>>koniec_przedzialu;
+    cout << "Podaj koniec przedzialu:";
+    cin >> koniec_przedzialu;
 
-    cout<<"Rozpoczecie losowania\n";
-    cout<<"Wylosowana zostala liczba : "<<losuj_z_przedzialu(poczatek_przedzialu,koniec_przedzialu);
+    cout << "Rozpoczecie losowania\n";
+    cout << "Wylosowana zostala liczba : " << losuj_z_przedzialu(poczatek_przedzialu, koniec_przedzialu);
 }
 
-int losuj_z_przedzialu(int poczatek, int koniec)
-{
+int losuj_z_przedzialu(int poczatek, int koniec) {
     srand(time(NULL));
-    return (rand()%koniec+poczatek);
+    return (rand() % koniec + poczatek);
 }
