4d696e6ac3c50ceabdeaf057b7be5590859b7d31
Piotr Ciosmak <MrChick888@gmail.com>
2021-10-19 22:55:43 +0200

    zadanie zrobione

--- Lab 3 - Zadanie 7/main.cpp
+++ Lab 3 - Zadanie 7/main.cpp
@@ -8,16 +8,13 @@ int main() {
     double a{20}, b{-20.3};
     sortowanie(&a, &b);
 }
-void sortowanie(double *x, double *y)
-{
-    if (*x>=*y)
-    {
-        cout<<"1. "<<*x<<endl;
-        cout<<"2. "<<*y<<endl;
-    }
-    else
-    {
-        cout<<"1. "<<*y<<endl;
-        cout<<"2. "<<*x<<endl;
+
+void sortowanie(double *x, double *y) {
+    if (*x >= *y) {
+        cout << "1. " << *x << endl;
+        cout << "2. " << *y << endl;
+    } else {
+        cout << "1. " << *y << endl;
+        cout << "2. " << *x << endl;
     }
 }
